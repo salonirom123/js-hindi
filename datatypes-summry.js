219663acfd326cabc8a2f98c01042a2b4a70bd9a
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-11-29 13:30:02 +0000

    datatypes-summry

---
+++ datatypes-summry.js
@@ -0,0 +1,37 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =  null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 34562938274949n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions,  
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "salu",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+// link
