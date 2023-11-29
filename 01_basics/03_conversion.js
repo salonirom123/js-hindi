34cdf5435a7b2b338005ba53ecf13fd72a079e05
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-11-29 12:05:32 +0000

    conversion and datatypes

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -23,5 +23,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let somNumber = 33
 
 let stringNum = String(somNumber)
-console.log(stringNum);  //33
-console.log(typeof stringNum); //string
+// console.log(stringNum);  //33
+// console.log(typeof stringNum); //string
+
+// **********************************OPRATIONS*******************************
+
+let val = 3
+let negval = -val
+// console.log(negval);
+
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2%2);
+
+let s1 = "hello"
+let s2 = " salu"
+
+let s3 = s1 + s2
+// console.log(s3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(2 + 3 *4 / 5 % 7);
+
+// console.log(+true);
+// console.log(+"");
+
+let n1, n2, n3
+n1 = n2 = n3 = 2 + 2
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+// link to study
+
+
