cb450e6b034526db1f09e8ab89d4f7c342f0e8cd
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-11-29 10:56:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score = "salu"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" =>NaN
+// true=> 1,false=>0
+
+
+let isLoggedIn = "salu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0=> false
+// "" => false
+// "salu" => true
+
+let somNumber = 33
+
+let stringNum = String(somNumber)
+console.log(stringNum);  //33
+console.log(typeof stringNum); //string
