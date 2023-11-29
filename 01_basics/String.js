a8f4e3ccbbaa01c426f0340f965330d692b77ebf
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-11-29 16:20:48 +0000

    String

---
+++ 01_basics/String.js
@@ -0,0 +1,39 @@
+const name = "hitesh"
+const repocount = 50
+
+// console.log(name + repocount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String('hetishi-maa-ddd')
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+
+// console.log(gamename.toUpperCase());
+
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('t'));
+
+const newString = gamename.substring(0,4)
+console.log(newString);
+
+const anotherString = gamename.slice(-6, 4)
+console.log(anotherString);
+
+const newString1 = "   hitesh   "
+console.log(newString1);
+console.log(newString1.trim());
+const url = "https://hiriolwej.com/hrtrr%20vjfvgg"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes("hitesh"))
+
+console.log(gamename.split('-'));
+
+console.log(gamename.search('h'));
+// console.log(gamename.blink());
+
+// console.log(gamename.bold());
+// console.log(gamename.fixed());
