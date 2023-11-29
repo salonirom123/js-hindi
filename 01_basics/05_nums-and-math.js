ee41fd85469f085bc62adfae5b49d22402e7f0e0
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-11-29 17:11:04 +0000

    math & num

---
+++ 01_basics/05_nums-and-math.js
@@ -0,0 +1,31 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernum = 123.3289
+
+// console.log(othernum.toPrecision(4));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ************************maths***********************************************
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.3));
+// console.log(Math.ceil(4.3));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
