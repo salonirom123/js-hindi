1ffa1e05fdde07e51026ceed6b672494bfe4d9f6
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-12-01 15:34:31 +0000

    truthy and falsy values

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,49 @@
+const userEmail = "salooer!234#"
+
+// if (userEmail) {
+//     console.log(`got user email`);
+// } else {
+//     console.log(`dont got uuser mail`);
+// }
+
+// falsy values
+
+// false => 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values 
+
+// "0", `false`, " ", [], {}, function(){}
+
+// userEmail = []
+// if (userEmail.length === 0) {
+//     console.log(`Array is empty`);
+// }
+
+const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0){
+//     console.log(`Object is empty`);
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 13
+// val1 = null ?? 10 ?? 30
+
+
+// console.log(val1);
+
+
+// Terniry operator
+
+// condition ? true :false 
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log(`less than 80`) : console.log(`greater than 80`);
+
+
+
