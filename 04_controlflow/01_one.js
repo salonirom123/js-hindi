aa745fb31033db89a3c4bd062e69cbb39c7c6d47
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-12-01 15:00:44 +0000

    controlflow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,45 @@
+// if
+// const isUserloggedIn = true
+const temp = 41
+
+// if (temp === 40) {
+//     console.log(`less than 50`);
+// }else{
+//     console.log(`greater than 50`);
+// }
+
+// console.log(`execute`);
+// <, >, <=, >=, ==, !=, ===
+
+const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if (balance > 500) {
+//     console.log("less than");
+// }else if (balance < 700) {
+//     console.log("less than 700 ");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// }else{
+//     console.log(`less than 1200`);
+// }
+
+const isUserloggedIn = true
+const debiteCard = true
+const loggedinFromGoogle = false
+const loggedinFromEmail = true
+
+if (isUserloggedIn && debiteCard || loggedinFromEmail) {
+    console.log(`allow to shopping`);
+}
