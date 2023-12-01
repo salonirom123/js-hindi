0b768f54d9ec4ee08431c4e1431d793d2e514b74
salonirom123 <139640095+salonirom123@users.noreply.github.com>
2023-12-01 15:34:04 +0000

    switch

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+   case "fab":
+         console.log("fab");
+         break;    
+   case "march":
+       console.log("march");
+       break;
+  case "april":
+        console.log("april");
+        break;
+    default:
+        break;
+}
